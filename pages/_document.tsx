93c11d869402c6895f9dc6090ebdc520c13b61ab
tao luo <taoluo@usc.edu>
2021-09-13 08:46:03 -0700

    update pwa

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,11 +27,7 @@ export default class extends Document {
           {/* TIP: set viewport head meta tag in _app.js, otherwise it will show a warning */}
           {/* <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' /> */}
 
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/icons/apple-touch-icon.png"
-          />
+          <link rel="icon" sizes="512x512" href="/icon.png" />
           <link rel="manifest" href="/manifest.json" />
           <link rel="shortcut icon" href="/favicon.ico" />
           <style>{`
